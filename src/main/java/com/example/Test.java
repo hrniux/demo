38be84a8644fd1c5e9004cb75b2d104713aa1dc9
hrniu <hrniuchina@gmail.com>
2025-07-01 19:17:10 +0800

    demo1

--- src/main/java/com/example/Test.java
+++ src/main/java/com/example/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("SnowFlake ID Generation Test");
         System.out.println("Generating IDs using SnowFlake algorithm...");
+        System.out.println("----------------------------------");
     }
 }
