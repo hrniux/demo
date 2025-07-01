d8acb8d5904a9b6e53b268df9c24823a0a0d304a
hrniu <hrniuchina@gmail.com>
2025-07-01 19:17:16 +0800

    demo1

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -4,5 +4,6 @@ public class Car {
     public static void main(String[] args) {
         System.out.println("Car ID Generation Test");
         System.out.println("Generating IDs for cars...");
+        System.out.println("----------------------------------");
     }
 }
